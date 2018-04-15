a32592239958b1542a2c11724d572d5e595f4490
Evan Shoshan <evan.shoshan@gmail.com>
2018-04-15 15:37:51 -0400

    add files

---
+++ Tutorial-Project/script.js
@@ -0,0 +1,15 @@
+﻿function toggleCollapse() {
+    var x = document.getElementById("nav");
+    if (x.className === "navCollapsed") {
+        x.className = "";
+    }
+    else {
+        x.className = "navCollapsed";
+    }
+    
+}
+
+function collapse() {
+    var x = document.getElementById("nav");
+    x.className = "navCollapsed";
+}
